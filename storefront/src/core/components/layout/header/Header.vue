bd8d285de4e22a876b736b3d030ab70702985c9a
gerasem <sougift@gmail.com>
2025-02-01 00:46:47 +0100

    header responsive menu

--- storefront/src/core/components/layout/header/Header.vue
+++ storefront/src/core/components/layout/header/Header.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import HeaderButtons from '@/core/components/layout/header/HeaderButtons.vue'
 import HeaderLogo from '@/core/components/layout/header/HeaderLogo.vue'
-import HeaderIcons from '@/core/components/layout/header/HeaderIcons.vue'
 import HeaderMenu from '@/core/components/layout/header/HeaderMenu.vue'
 import HeaderLanguage from '@/core/components/layout/header/LanguageSwitcher.vue'
 import InformationBanner from '@/core/components/layout/header/InformationBanner.vue'
 import HeaderBurger from '@/core/components/layout/header/HeaderBurger.vue'
 import { ref } from 'vue'
+import HeaderCart from '@/core/components/layout/header/HeaderCart.vue'
 
 const mobileMenu = ref<boolean>(false)
 
@@ -20,29 +20,33 @@ const toggleMenu = () => {
     <InformationBanner class="is-hidden-mobile" />
   </header>
 
-  <nav class="navbar is-flex container is-fluid" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-flex container is-fluid" :class="{'is-flex-direction-column': mobileMenu}" role="navigation"
+       aria-label="main navigation">
     <div class="navbar-brand header__navbar-brand">
-
-      <HeaderBurger :mobileMenu="mobileMenu" @toggleMenu="toggleMenu()"/>
+      <HeaderBurger :mobileMenu="mobileMenu" @toggleMenu="toggleMenu()" />
 
       <HeaderLogo />
+
+      <div class="navbar-item is-hidden-tablet">
+        <HeaderCart /> 2
+      </div>
     </div>
 
-    <div class="navbar-start is-flex-grow-1 is-align-items-center is-hidden is-flex-tablet">
+    <div class="navbar-start is-flex-grow-1 is-align-items-center" :class="{'is-hidden is-flex-tablet': !mobileMenu}">
       <HeaderMenu />
     </div>
 
     <div class="navbar-end is-flex is-align-items-center">
-      <div class="navbar-item is-hidden is-flex-tablet">
+      <div class="navbar-item" :class="{'is-hidden is-flex-tablet': !mobileMenu}">
         <HeaderLanguage />
       </div>
 
-      <div class="navbar-item is-hidden is-flex-tablet">
+      <div class="navbar-item" :class="{'is-hidden is-flex-tablet': !mobileMenu}">
         <HeaderButtons />
       </div>
 
-      <div class="navbar-item">
-        <HeaderIcons />
+      <div class="navbar-item is-hidden is-flex-tablet">
+        <HeaderCart /> 1
       </div>
     </div>
   </nav>
--- storefront/src/core/components/layout/header/HeaderCart.vue
+++ storefront/src/core/components/layout/header/HeaderCart.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 import BaseIcon from '@/core/components/common/BaseIcon.vue'
 import { localePath } from '@/core/composables/localePath.ts'
-
-const icons = [
-  { icon: 'bag', link: 'cart' },
-]
 </script>
 
 <template>
-  <RouterLink v-for="icon in icons"
-              :key="icon.link"
-              class="header__icon-link"
-              :to="localePath(icon.link)">
-    <BaseIcon :icon="icon.icon" :width="24" :height="24" />
+  <RouterLink class="header__icon-link"
+              :to="localePath('cart')">
+    <BaseIcon icon="bag" :width="24" :height="24" />
   </RouterLink>
 </template>
 
