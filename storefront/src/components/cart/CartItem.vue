770344fdb99d2983d40736a952a223a79c31a0d3
gerasem <sougift@gmail.com>
2025-02-09 00:20:27 +0100

    shopping cart base html and css

---
+++ storefront/src/components/cart/CartItem.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts">
+import { localePath } from '@/composables/localePath'
+import BaseIcon from '@/components/common/BaseIcon.vue'
+
+defineProps<{
+  item: Object;
+}>()
+
+const deleteItemWithConfirm = (event: Event, itemId: string) => {
+
+}
+
+const changeItemCount = (count: number, item) => {
+
+}
+</script>
+
+<template>
+  <div class="cart__item is-flex">
+    <RouterLink :to="localePath(`item`)">
+      <div class="cart__image-container">
+        <img
+          v-if="true"
+          :src="'https://placehold.co/400x400'"
+          :alt="'item'"
+          class="cart__image is-block"
+        />
+      </div>
+    </RouterLink>
+
+    <div class="cart__main">
+      <div class="cart__prices is-flex">
+        <div class="cart__price" v-if="true">
+          {{ 1100 }}
+          <span>x {{ 1 }}</span>
+          {{ 1100 }}
+        </div>
+
+        <div class="cart__old-price" v-if="true">
+          {{ 1200 }}
+        </div>
+
+        <div class="cart__discount" v-if="false">%</div>
+      </div>
+
+      <h4 class="cart__title">Item 123</h4>
+    </div>
+
+    <input class="input" style="width: 100px;" type="text" placeholder="Text input" />
+
+    <BaseIcon :width="25" :height="25" icon="x-lg" @click="deleteItemWithConfirm($event, 1)" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cart {
+  &__item {
+    align-items: center;
+    padding: 1rem 1rem 1rem 0;
+    position: relative;
+    user-select: none;
+
+    &:not(:last-of-type):after {
+      position: absolute;
+      content: "";
+      width: calc(100% - 2rem);
+      left: 1rem;
+      bottom: 0;
+      height: 1px;
+      background: $background-gray;
+    }
+  }
+
+  &__image-container {
+    overflow: hidden;
+  }
+
+  &__image {
+    width: 200px;
+    height: 150px;
+    object-fit: cover;
+    background-color: $background-gray;
+    transition: 0.2s;
+
+    @media (max-width: $screen-lg-max) {
+      width: 130px;
+      height: 100px;
+    }
+
+    @media (max-width: $screen-md-max) {
+      width: 100px;
+      height: 75px;
+    }
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  &__button {
+    cursor: pointer;
+    margin-top: 2rem;
+  }
+
+  &__main {
+    flex: 1;
+    margin: 0 2rem;
+  }
+
+  &__price {
+    font-size: 1.125rem;
+    font-weight: 600;
+
+    span {
+      font-weight: 400;
+      font-size: 1rem;
+      margin: 0 20px;
+    }
+  }
+
+  &__old-price {
+    text-decoration: line-through;
+    margin: 0 2rem;
+  }
+
+  &__discount {
+    color: $color-primary;
+  }
+
+  &__title {
+    font-size: 1rem;
+  }
+
+  &__actions {
+    align-items: center;
+  }
+}
+</style>
--- storefront/src/views/CartView.vue
+++ storefront/src/views/CartView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import Footer from '@/components/footer/Footer.vue'
-import Hero from '@/components/content/Hero.vue'
 import InformationBanner from '@/components/information-banner/InformationBanner.vue'
 import Navbar from '@/components/navbar/Navbar.vue'
-import { useMeta } from '@/composables/useMeta.ts'
+import CartItem from '@/components/cart/CartItem.vue'
 
-useMeta("Cart", "cart description")
+import { useMeta } from '@/composables/useMeta.ts'
+import { onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+useMeta('Cart', 'cart description')
+
+const { t } = useI18n()
+
+onMounted(async () => {
+
+})
+
+const deleteItemsFromCart = () => {
+
+}
 </script>
 
 <template>
@@ -13,17 +26,134 @@ useMeta("Cart", "cart description")
 
   <Navbar />
 
-  <Hero title="Catalog" />
+  <div class="container is-fluid">
+    <template v-if="true">
+      <div class="columns">
+        <div class="column is-two-thirds">
+          <h2 class="title is-2">Shopping Cart</h2>
 
-  <section class="section">
-    <div class="container is-fluid">
-      Shopping Cart
-    </div>
-  </section>
+          <template v-for="item in 5" :key="item">
+            <CartItem  item=""/>
+          </template>
+        </div>
+
+        <div class="column is-one-third">
+          <div class="cart__form">
+            <div class="cart__info">
+              {{ t('Free Shipping on all orders over 50€') }}
+            </div>
+
+            <div class="cart__info cart__info--error">Coupon Error</div>
+
+            <div class="cart__form-container">
+              <div class="has-text-centered">
+                <div class="columns">
+                  <div class="column">{{ t('Sub-Total') }}:</div>
+                  <div class="column">
+                    <span class="cart__form-price">{{ totalPrice }}</span>
+                  </div>
+                </div>
+
+                <div class="columns">
+                  <div class="column">{{ t('Shipping') }}:</div>
+                  <div class="column">
+                    <span class="cart__form-price">
+                      5€
+                    </span>
+                  </div>
+                </div>
+
+                <div class="columns" v-if="true">
+                  <div class="column cart__form-price--discount">
+                    Discount ():
+                  </div>
+                  <div class="column">
+                    <span class="cart__form-price cart__form-price--discount">
+                      {{ 0 }}
+                    </span>
+                  </div>
+                </div>
+
+                <div class="columns">
+                  <div class="column">
+                    {{ t('Total price') }}:
+                  </div>
+                  <div class="column">
+                    <span class="cart__form-price cart__form-price--total">
+                      {{ totalPrice }}
+                    </span>
+                  </div>
+                </div>
+
+                <div class="columns">
+                  <div class="column">
+
+                  </div>
+                </div>
+
+                <div class="columns">
+                  <div class="column">
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <template v-else>
+      <h2 class="title is-2">Shopping Cart</h2>
+
+      <p>{{ t('Shopping cart is empty') }}</p>
+    </template>
+  </div>
 
   <Footer />
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+.cart {
+  &__form {
+    position: sticky;
+    top: 40px;
+    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.05);
+    margin-top: 30px;
+    @media (max-width: $screen-lg-max) {
+      margin: 2rem 0;
+    }
+  }
 
+  &__form-container {
+    padding: 1.5rem;
+  }
+
+  &__form-price {
+    font-weight: 600;
+
+    &--total {
+      color: $color-secondary;
+    }
+
+    &--discount {
+      color: $color-primary;
+    }
+  }
+
+  &__info {
+    background-color: rgb(255, 249, 241);
+    padding: 1rem 1.5rem;
+    text-align: center;
+    color: $color-secondary;
+    font-weight: 600;
+    line-height: 24px;
+
+    &--error {
+      background-color: rgba(235, 62, 125, 0.25);
+      color: $color-primary;
+    }
+  }
+}
 </style>
+
