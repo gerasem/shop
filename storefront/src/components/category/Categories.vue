5246fa2b205f17c20d4a0c602ae0c59a51d862df
gerasem <sougift@gmail.com>
2025-02-13 21:12:35 +0100

    fix categories narrow on the main page

--- storefront/src/components/category/Categories.vue
+++ storefront/src/components/category/Categories.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import CategoryCard from '@/components/category/CategoryCard.vue'
 import CategoryPreview from '@/components/category/CategoryPreview.vue'
+import CategoriesNarrow from '@/components/category/CategoriesNarrow.vue'
+import { useDevice } from '@/composables/useDevice.ts'
+
+const { isMobile } = useDevice()
 </script>
 
 <template>
+  <CategoriesNarrow v-if="isMobile"/>
+
   <div class="container is-fluid">
     <div class="columns is-5-tablet is-6-desktop is-8-fullhd">
-      <div class="column is-full is-one-third-tablet is-one-fifth-desktop category__container">
-        <div class="is-hidden-mobile category__sticky-header"></div>
+      <div v-if="!isMobile" class="column is-one-third-tablet is-one-fifth-desktop">
         <CategoryCard v-for="category in 5" :key="category" />
       </div>
 
@@ -19,26 +24,5 @@ import CategoryPreview from '@/components/category/CategoryPreview.vue'
 </template>
 
 <style scoped lang="scss">
-.category {
-  &__container {
-    @media (max-width: $screen-md-max) {
-      display: flex;
-      justify-content: space-around;
-      flex-wrap: wrap;
-      width: 100%;
-      background: $color-categories-background;
-      gap: 15px;
-    }
-  }
 
-  &__sticky-header {
-    /*background: #ffffff;
-    z-index: 5;
-    width: 100%;
-    height: 20px;
-    top: 56px;
-    position: sticky;
-    margin-top: -20px;*/
-  }
-}
 </style>
