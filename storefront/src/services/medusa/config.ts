f1d9b2ce695216a9a1f656003306b79fa4ecc374
gerasem <sougift@gmail.com>
2025-02-27 11:38:11 +0100

    storefront: congig file for api connection (medusa); backend update package.json

---
+++ storefront/src/services/medusa/config.ts
@@ -0,0 +1,15 @@
+import Medusa from "@medusajs/js-sdk"
+
+let MEDUSA_BACKEND_URL = "http://localhost:9000"
+
+if (import.meta.env.VITE_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+}
+
+export const sdk = new Medusa({
+  baseUrl: MEDUSA_BACKEND_URL,
+  debug: import.meta.env.MODE === 'development',
+  publishableKey: import.meta.env.VITE_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "",
+})
+
+console.log("sdk", sdk)
