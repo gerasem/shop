a5dd16f29e9581d59019de3cb19e8269b0840e03
gerasem <sougift@gmail.com>
2025-05-22 22:46:28 +0200

    refactoring image component: support png and jpg formats

--- storefront/src/components/media/Image.vue
+++ storefront/src/components/media/Image.vue
@@ -5,12 +5,12 @@ const props = defineProps<{
   src: string
 }>()
 
-const imageName = computed(() => {
+const imageName = computed((): string => {
   const parts = props.src.split('.')
   return parts.length > 1 ? parts.slice(0, -1).join('.') : props.src
 })
 
-const imageExtension = computed(() => {
+const imageExtension = computed((): string => {
   const parts = props.src.split('.')
   return parts.length > 1 ? parts.pop() : ''
 })
@@ -19,12 +19,26 @@ const loadImage = defineAsyncComponent({
   loader: () => import(`@/assets/images/${imageName.value}.${imageExtension.value}`),
   errorComponent: defineAsyncComponent(() => import(`@/assets/images/_default-image.svg`)),
 })
+
+const imageUrl = computed((): string => {
+  const path = new URL(`../../assets/images/${props.src}`, import.meta.url).href
+  if (path.endsWith('/undefined')) {
+    return new URL(`../../assets/images/_default-image.svg`, import.meta.url).href
+  } else {
+    return new URL(`../../assets/images/${props.src}`, import.meta.url).href
+  }
+})
 </script>
 
 <template>
   <component
+    v-if="imageExtension === 'svg'"
     :is="loadImage"
     class="image"
+  />
+  <img
+    v-else
+    :src="imageUrl"
   />
 </template>
 
