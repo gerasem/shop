2a37cf4cd899f34a9b5d2785d0a26ba04e8d1fef
gerasem <sougift@gmail.com>
2025-07-06 23:14:16 +0200

    add scroll behavior

--- storefront/src/router/index.ts
+++ storefront/src/router/index.ts
@@ -3,6 +3,13 @@ import { handleRouting } from './beforeEnter'
 import HomeView from '@/views/HomeView.vue'
 const router: Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     {
       path: '/:locale(en|de)?',
