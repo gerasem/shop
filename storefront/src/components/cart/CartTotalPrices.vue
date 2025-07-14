3d71606b8d461da06cabeadbbff5bf40bc89b500
gerasem <sougift@gmail.com>
2025-07-14 22:50:22 +0200

    refactoring: CartTotalPrices component

---
+++ storefront/src/components/cart/CartTotalPrices.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { localePath } from '@/composables/localePath.ts'
+import { useI18n } from 'vue-i18n'
+import CartCalculatedPrice from '@/components/cart/CartCalculatedPrice.vue'
+import Button from '@/components/form/Button.vue'
+import { useCartStore } from '@/stores/CartStore'
+
+const cartStore = useCartStore()
+
+const { t } = useI18n()
+
+interface IButton {
+  name: string
+  icon: string
+  path: string
+}
+
+defineProps<{
+  button: IButton
+}>()
+</script>
+
+<template>
+  <div class="cart__form">
+    <div class="cart__info">
+      {{ t('Free Shipping on all orders over 50€') }}
+    </div>
+
+    <!-- <div
+            v-if="false"
+            class="cart__info cart__info--error"
+          >
+            Coupon Error
+          </div> -->
+
+    <div class="cart__form-container">
+      <div class="has-text-centered">
+        <CartCalculatedPrice
+          label="Sub Total"
+          :value="cartStore.cart?.subtotal"
+        />
+
+        <CartCalculatedPrice
+          label="Shipping"
+          :value="cartStore.cart?.shipping_total"
+        />
+
+        <CartCalculatedPrice
+          v-if="false"
+          class="cart--discount"
+          label="Discount"
+          :value="0"
+        />
+
+        <CartCalculatedPrice
+          class="cart--total"
+          label="Total price"
+          :value="cartStore.cart?.total"
+        />
+
+        <RouterLink :to="localePath(button.path)">
+          <Button
+            :icon="button.icon"
+            class="mt-4 is-primary is-fullwidth"
+            >{{ t(button.name) }}</Button
+          >
+        </RouterLink>
+
+        <div class="columns">
+          <div class="column"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cart {
+  &__form {
+    position: sticky;
+    top: 40px;
+    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.05);
+    margin-top: 30px;
+    @media (max-width: $screen-lg-max) {
+      margin: 2rem 0;
+    }
+  }
+
+  &__info {
+    background-color: rgb(255, 249, 241);
+    padding: 1rem 1.5rem;
+    text-align: center;
+    color: $color-secondary;
+    font-weight: 600;
+    line-height: 24px;
+
+    &--error {
+      background-color: rgba(235, 62, 125, 0.25);
+      color: $color-primary;
+    }
+  }
+
+  &__form-container {
+    padding: 1.5rem;
+  }
+}
+</style>
--- storefront/src/views/CartView.vue
+++ storefront/src/views/CartView.vue
@@ -6,10 +6,8 @@ import { useLoaderStore } from '@/stores/LoaderStore'
 import Header from '@/components/content/Header.vue'
 import { useCartStore } from '@/stores/CartStore'
 import { computed } from 'vue'
-import CartCalculatedPrice from '@/components/cart/CartCalculatedPrice.vue'
-import Button from '@/components/form/Button.vue'
 import CartSteps from '@/components/cart/CartSteps.vue'
-import { localePath } from '@/composables/localePath.ts'
+import CartTotalPrices from '@/components/cart/CartTotalPrices.vue'
 
 const cartStore = useCartStore()
 const loaderStore = useLoaderStore()
@@ -50,90 +48,10 @@ useSeoMeta({
       </div>
 
       <div class="column is-one-third">
-        <div class="cart__form">
-          <div class="cart__info">
-            {{ t('Free Shipping on all orders over 50€') }}
-          </div>
-
-          <!-- <div
-            v-if="false"
-            class="cart__info cart__info--error"
-          >
-            Coupon Error
-          </div> -->
-
-          <div class="cart__form-container">
-            <div class="has-text-centered">
-              <CartCalculatedPrice
-                label="Sub Total"
-                :value="cartStore.cart?.subtotal"
-              />
-
-              <CartCalculatedPrice
-                label="Shipping"
-                :value="cartStore.cart?.shipping_total"
-              />
-
-              <CartCalculatedPrice
-                v-if="false"
-                class="cart--discount"
-                label="Discount"
-                :value="0"
-              />
-
-              <CartCalculatedPrice
-                class="cart--total"
-                label="Total price"
-                :value="cartStore.cart?.total"
-              />
-
-              <RouterLink :to="localePath(`checkout`)">
-                <Button
-                  icon="bag"
-                  class="mt-4 is-primary is-fullwidth"
-                  >{{ t('Checkout') }}</Button
-                >
-              </RouterLink>
-
-              <div class="columns">
-                <div class="column"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CartTotalPrices :button="{ name: 'Weiter', icon: 'bag', path: 'checkout' }" />
       </div>
     </div>
   </main>
 </template>
 
-<style lang="scss" scoped>
-.cart {
-  &__form {
-    position: sticky;
-    top: 40px;
-    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.05);
-    margin-top: 30px;
-    @media (max-width: $screen-lg-max) {
-      margin: 2rem 0;
-    }
-  }
-
-  &__form-container {
-    padding: 1.5rem;
-  }
-
-  &__info {
-    background-color: rgb(255, 249, 241);
-    padding: 1rem 1.5rem;
-    text-align: center;
-    color: $color-secondary;
-    font-weight: 600;
-    line-height: 24px;
-
-    &--error {
-      background-color: rgba(235, 62, 125, 0.25);
-      color: $color-primary;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
