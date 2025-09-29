75b92d67a61aa27e55ba1119b13b581845836261
gerasem <sougift@gmail.com>
2025-09-29 22:06:48 +0200

    animated changing of the price

--- storefront/src/components/cart/CartCalculatedPrice.vue
+++ storefront/src/components/cart/CartCalculatedPrice.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
-import { convertToLocale } from '@/utils/priceUtils'
+import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { convertToLocale } from '@/utils/priceUtils'
 
 const { t } = useI18n()
 
-defineProps<{
+const props = defineProps<{
   value: number | undefined
   label: string
 }>()
+
+const animatedValue = ref<number | undefined>(props.value)
+
+const animatePrice = (start: number, end: number, duration: number = 1000) => {
+  let startTime: number | null = null
+  const step = (timestamp: number) => {
+    if (!startTime) startTime = timestamp
+    const progress = Math.min((timestamp - startTime) / duration, 1)
+    animatedValue.value = Math.round(start + (end - start) * progress)
+    if (progress < 1) {
+      requestAnimationFrame(step)
+    }
+  }
+  requestAnimationFrame(step)
+}
+
+watch(
+  () => props.value,
+  (newValue, oldValue) => {
+    if (newValue !== undefined && oldValue !== undefined) {
+      animatePrice(oldValue, newValue)
+    } else {
+      animatedValue.value = newValue
+    }
+  },
+)
 </script>
 
 <template>
@@ -15,11 +42,10 @@ defineProps<{
     <div class="column">{{ t(label) }}:</div>
     <div class="column">
       <span
-        v-if="value !== undefined"
+        v-if="animatedValue !== undefined"
         class="cart__form-price"
-        >{{ convertToLocale({ amount: value }) }}</span
+        >{{ convertToLocale({ amount: animatedValue }) }}</span
       >
-
       <span v-else> - </span>
     </div>
   </div>
@@ -33,6 +59,8 @@ defineProps<{
 
   &__form-price {
     font-weight: 600;
+    display: inline-block;
+    transition: all 0.3s ease;
   }
 
   &--total .cart__form-price {
